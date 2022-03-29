08aa2eaa26fbc266618a89c697c2e19f27e8aecf
Kevin Nguyen <kevin.nguyen6079@gmail.com>
2022-03-29 22:48:29 +0000

    added other query

--- handleForm.php
+++ handleForm.php
@@ -9,32 +9,32 @@ $device_type = $_POST['device_type'];
 $manufacturer = $_POST['manufacturer'];
 $serial_number = "%{$_POST['serial_number']}%";
 
-if (isset($_POST['device_type'])) {
-    $sql = "SELECT * from devices where device_type = ? limit 10";
+if (isset($device_type) && isset($manufacturer) && isset($serial_number)) {
+    $sql = "SELECT * from devices where manufacturer = ? and device_type = ? and serial_number like ? limit 10";
+
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "sss", $manufacturer, $device_type, $serial_number);
+} else if (isset($device_type) && isset($manufacturer)) {
+    $sql = "SELECT * from devices where manufacturer = ? and device_type = ?  limit 10";
+
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $manufacturer, $device_type);
+} else if (isset($device_type)) {
+    $sql = "SELECT * from devices where device_type = ?  limit 10";
 
     mysqli_stmt_prepare($stmt, $sql);
     mysqli_stmt_bind_param($stmt, "s", $device_type);
-}
-if (isset($_POST['manufacturer'])) {
-    $sql = "SELECT * from devices where manufacturer = ? limit 10";
+} else if (isset($manufacturer)) {
+    $sql = "SELECT * from devices where manufacturer = ?  limit 10";
 
     mysqli_stmt_prepare($stmt, $sql);
     mysqli_stmt_bind_param($stmt, "s", $manufacturer);
-}
-if (isset($_POST['serial_number'])) {
-    $sql = "SELECT * from devices where serial_number like ? limit 10";
+} else if (isset($serial_number)) {
+    $sql = "SELECT * from devices where serial_number like  ?  limit 10";
 
     mysqli_stmt_prepare($stmt, $sql);
     mysqli_stmt_bind_param($stmt, "s", $serial_number);
 }
-// if (isset($_POST['manufacturer']) and isset($_POST['device_type'])) {
-//     $manufacturer = $_POST['manufacturer'];
-//     $device_type = $_POST['device_type'];
-//     $sql = "SELECT * from devices where manufacturer = ? and device_type = ? limit 10";
-
-//     mysqli_stmt_prepare($stmt, $sql);
-//     mysqli_stmt_bind_param($stmt, "ss", $manufacturer, $device_type);
-// }
 if (!mysqli_stmt_execute($stmt))
     exit(mysqli_stmt_error($stmt));
 
