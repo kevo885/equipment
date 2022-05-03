770aca2ee196c7b6548e440b74b470b5b973cea8
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2022-05-03 01:56:42 -0500

    added add device api

--- api/addDevice.php
+++ api/addDevice.php
@@ -1,44 +1,50 @@
 <?php
 include_once "../inc/.env.php";
-$id = $_REQUEST['id'];
-// has to be a positive int
-if (!is_numeric($id) && $id != NULL) {
-    header('Content-Type: application/json');
-    header('HTTP/1.1 200 OK');
-    echo json_encode(array("Status: Invalid ID", "Device ID must be a positive integer"), JSON_PRETTY_PRINT);
-    die();
-}
+$device_type = $_REQUEST['device_type'];
+$manufacturer = $_REQUEST['manufacturer'];
+$serial_number = $_REQUEST['sn'];
+
 // if blank
-elseif ($id == NULL) {
+if ($device_type == NULL || $manufacturer == NULL || $serial_number == NULL) {
     header('Content-Type: application/json');
     header('HTTP/1.1 200 OK');
-    echo json_encode(array("Status: Invalid ID", "Device ID must not be blank"), JSON_PRETTY_PRINT);
+    echo json_encode(array("Status: No input", "Device type, Manufactuer, and serial number cannot be blank"), JSON_PRETTY_PRINT);
     die();
 }
 // add device
-else {
-    $sql = "Select * from `devices` where `id`= ?";
-    mysqli_stmt_prepare($stmt, $sql);
-    mysqli_stmt_bind_param($stmt, 'i', $id);
+// checks if serial_number exists already
+$check_serial_number = "SELECT * FROM devices WHERE serial_number=?";
 
-    // execute query and bind query
-    if (!mysqli_stmt_execute($stmt))
-        exit(mysqli_stmt_error($stmt));
-    mysqli_stmt_bind_result($stmt, $id, $device_type, $manufacturer, $sn, $status);
-    mysqli_stmt_store_result($stmt);
-    // get device
-    if (mysqli_stmt_num_rows($stmt) > 0) {
-        while (mysqli_stmt_fetch($stmt)) {
-            header('Content-Type: application/json');
-            header('HTTP/1.1 200 OK');
-            $output = array("Status: OK", "Device Info: ", "Device ID: $id", 'Maufacturer:' . $manufacturer, 'Device Type:' . $device_type, 'Serial Number:' . $sn, 'Status:' . $status);
-            echo json_encode($output, JSON_PRETTY_PRINT);
-        }
+// preparing statement
+mysqli_stmt_prepare($stmt, $check_serial_number);
+mysqli_stmt_bind_param($stmt, 's', $serial_number);
+
+if (!mysqli_stmt_execute($stmt))
+    exit(mysqli_stmt_error($stmt));
+
+mysqli_stmt_store_result($stmt);
+
+// if serial_number is taken, print error 
+if (mysqli_stmt_num_rows($stmt) > 0) {
+    header('Content-Type: application/json');
+    header('HTTP/1.1 200 OK');
+    echo json_encode(array("Status: ERROR", "MSG: Serial number already exists"), JSON_PRETTY_PRINT);
+    die();
+}
+// if  serial_number isn't taken, insert into database
+else {
+    $insert = "INSERT INTO devices (device_type,manufacturer,serial_number) values (?,?,?)";
+
+    // prepare for insert
+    mysqli_stmt_prepare($stmt, $insert);
+    mysqli_stmt_bind_param($stmt, "sss", $device_type, $manufacturer, $serial_number);
+
+    if (!mysqli_stmt_execute($stmt)) {
+        mysqli_stmt_error($stmt);
+        die();
     }
-    // no device
-    else {
-        header('Content-Type: application/json');
-        header('HTTP/1.1 200 OK');
-        echo json_encode(array("Status: Not Found", "MSG: Device Id: $id not in database"), JSON_PRETTY_PRINT);
-    }
+    header('Content-Type: application/json');
+    header('HTTP/1.1 200 OK');
+    $output = array("Status: OK", "Device Info: ", "Device type: $device_type", 'Maufacturer:' . $manufacturer, 'Serial Number:' . $serial_number);
+    echo json_encode($output, JSON_PRETTY_PRINT);
 }
